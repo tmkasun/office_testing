56b3c71bb1e80fa93d776bf0817205af9aa20ca8
Kasun Thennakoon <tmkasun@gmail.com>
2018-01-03 09:16:11 +0530

    fix #1 issue thanks to @tmkasun :thumbsup: :v: wohaa :sunny:

---
+++ index.js
@@ -0,0 +1,4 @@
+import Commons from './libs/KnnectCommons'
+
+let sample = new Commons();
+sample.hello();
---
+++ libs/KnnectCommons.js
@@ -0,0 +1,12 @@
+class KnnectCommons {
+	constructor(){
+		console.log("Knnect constructor has been called!!!");
+	}
+
+	hello() {
+		console.log("Knnect commons hello method :)");
+		return "Hello";
+	}
+}
+
+export default KnnectCommons;
